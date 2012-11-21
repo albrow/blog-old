2614bf3d073d47cbf9284f776ea85378bbdfd42c
stephenalexbrowne <stephenalexbrowne@gmail.com>
2012-11-21 06:17:17 -0500

    Remove some old files

--- source/_posts/2012-11-21-another-post.markdown
+++
@@ -1,7 +0,0 @@
----
-layout: post
-title: "Another Post"
-date: 2012-11-21 02:36
----
-
-Holy shit this is another post!
--- source/javascripts/link_activator.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.nav > li').each(function() {
-    var href = $(this).find('a').attr('href');
-    if (href === window.location.pathname) {
-      $(this).addClass('active');
-    }
-  });
-});
